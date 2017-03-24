56f4cdc1346f54d187b3a77c9d12a60488b1522b
Shashank Shantam <fedshashank95@gmail.com>
2017-03-24 21:52:38 +0530

    Update logdata.py

--- logdata.py
+++ logdata.py
@@ -23,44 +23,53 @@ GPIO.setup(15,GPIO.IN)
 # Variables for MySQL
 db = MySQLdb.connect(host="localhost", user="root",passwd="deb280794", db="temp_database")
 cur = db.cursor()
- 
-def tempRead():
-    t = open(temp_sensor, 'r')
-    lines = t.readlines()
-    t.close()
- 
-    temp_output = lines[1].find('t=')
-    if temp_output != -1:
-        temp_string = lines[1].strip()[temp_output+2:]
-        temp_c = float(temp_string)/1000.0
-    return round(temp_c,1)
- 
-while True:
+
+#opening file to log data locally
+file = open("/home/pi/Desktop/sensordata.txt","rw+")
+start = time.time()
+while True:  
     x = ser.readline()
     f=x.split()
-    print f
     moisture = f[1]
     humidity = f[4]
     temp = f[7]
-    
     print moisture
     print humidity
     print temp
+    m = str(moisture)   
+    h = str(humidity)
+    t = str(temp)
     datetimeWrite = (time.strftime("%Y-%m-%d ") + time.strftime("%H:%M:%S"))
+    dtr = str(datetimeWrite)
+    file.write(dtr + "\t")
+    file.write(m + "\t")
+    file.write(h + "\t")
+    file.write(t + "\n")
+    
+    if time.time() - start < 900:
+        continue
+            
     #print datetimeWrite
-    sql = ("""INSERT INTO tempLog (datetime,temperature,humidity,moisture) VALUES (%s,%s,%s,%s)""",(datetimeWrite,temp,humidity,moisture))
-    try:
-        print "Writing to database..."
-        # Execute the SQL command
-        cur.execute(*sql)
-        # Commit your changes in the database
-        db.commit()
-        print "Write Complete"
- 
-    except:
-        # Rollback in case there is any error
-        db.rollback()
-        print "Failed writing to database"
+    for line in file:
+        data = line.split()
+        datetimeWrite = data[0]
+        moisture = data[1]
+        humidity = data[2]
+        temp = data[3]
+        sql = ("""INSERT INTO tempLog (datetime,temperature,humidity,moisture) VALUES (%s,%s,%s,%s)""",(datetimeWrite,temp,humidity,moisture))
+        try:
+            print "Writing to database..."
+            # Execute the SQL command
+            cur.execute(*sql)
+            # Commit your changes in the database
+            db.commit()
+            print "Write Complete"
+        except:
+            # Rollback in case there is any error
+            db.rollback()
+            print "Failed writing to database"
 
-    time.sleep(1)
+    #time.sleep(1)
+    file = open("/home/pi/Desktop/sensordata.txt","rw+")  
+    start = time.time()
     
